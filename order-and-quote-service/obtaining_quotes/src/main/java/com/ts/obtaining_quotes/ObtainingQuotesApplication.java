7888480cc14107cb68d9253994a9429ac74038f3
AlekseyG <gelvides.aleksey@outlook.com>
2022-05-11 21:25:31 +0700

    Сделал примерную структуру.

---
+++ order-and-quote-service/obtaining_quotes/src/main/java/com/ts/obtaining_quotes/ObtainingQuotesApplication.java
@@ -0,0 +1,12 @@
+package com.ts.obtaining_quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObtainingQuotesApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(ObtainingQuotesApplication.class, args);
+    }
+
+}
